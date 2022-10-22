a340ca9888414c5bdbf977de58d0a2dfefc7de41
Bartek Pacia <barpac02@gmail.com>
2022-10-22 12:22:39 +0200

    add integration test

---
+++ integration_test/widget_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_json_reporter_bug/main.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Counter', () {
+    testWidgets(
+      'is incremented when plus is tapped',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(const MyApp());
+
+        expect(find.text('0'), findsOneWidget);
+        expect(find.text('1'), findsNothing);
+
+        await tester.tap(find.byIcon(Icons.add));
+        await tester.pump();
+
+        expect(find.text('0'), findsNothing);
+        expect(find.text('1'), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'is decremented when minus is tapped',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(const MyApp());
+
+        expect(find.text('0'), findsOneWidget);
+        expect(find.text('-1'), findsNothing);
+
+        await tester.tap(find.byIcon(Icons.remove));
+        await tester.pump();
+
+        expect(find.text('0'), findsNothing);
+        expect(find.text('-1'), findsOneWidget);
+      },
+    );
+  });
+}
