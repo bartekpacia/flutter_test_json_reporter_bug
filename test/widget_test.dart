887f0d1640ed0a11c37e9fc1d9273322706f353d
Bartek Pacia <barpac02@gmail.com>
2022-10-22 16:18:49 +0200

    remove reports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@ void main() {
     testWidgets(
       'is incremented when plus is tapped',
       (WidgetTester tester) async {
-        await tester.pumpWidget(const MyApp());
+        await tester.pumpWidget(const MyApp(title: 'increment'));
 
         expect(find.text('0'), findsOneWidget);
         expect(find.text('1'), findsNothing);
@@ -23,7 +23,7 @@ void main() {
     testWidgets(
       'is decremented when minus is tapped',
       (WidgetTester tester) async {
-        await tester.pumpWidget(const MyApp());
+        await tester.pumpWidget(const MyApp(title: 'decrement'));
 
         expect(find.text('0'), findsOneWidget);
         expect(find.text('-1'), findsNothing);
